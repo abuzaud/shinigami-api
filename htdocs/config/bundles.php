0c755d1e6365cecff391843c1a16a1190abaa89d
Antoine Buzaud <antoine.buzaud@gmail.com>
2018-08-08 17:00:42 +0200

    Installation du knp_snappy pour la génération de pdf et d'image

--- htdocs/config/bundles.php
+++ htdocs/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
