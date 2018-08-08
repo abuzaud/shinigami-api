717c0e7123f442c70bb316970a653685a538484f
Antoine Buzaud <antoine.buzaud@gmail.com>
2018-08-08 18:33:41 +0200

    Changement de nom de fonction 'createCard' -> 'createCardFromEstablishmentCode'

--- htdocs/src/Controller/Card/CardCreate.php
+++ htdocs/src/Controller/Card/CardCreate.php
@@ -5,25 +5,36 @@ namespace App\Controller\Card;
 use App\Card\CardFactory;
 use App\Entity\Card;
 
+/**
+ * Class CardCreate
+ * @package App\Controller\Card
+ */
 class CardCreate
 {
     private $cf;
 
+    /**
+     * CardCreate constructor.
+     * @param CardFactory $cardFactory
+     */
     public function __construct(CardFactory $cardFactory)
     {
         $this->cf = $cardFactory;
     }
 
+    /**
+     * Créer une carte à partir du code de l'établissement
+     * @param Card $data
+     * @return Card
+     */
     public function __invoke(Card $data): Card
     {
-        /** @var Card $data */
-        # TODO : Finir ce controller + test unitaire + gestion des exceptions
-        # Génère le nouveau code de la carte de fidélité
-
+        # Récupération du code de l'établissement
         $codeEstablishment = $data->getEstablishmentCode();
 
+        # Création de la carte à partir du code de l'établissement
         try {
-            $data = $this->cf->createCard($codeEstablishment);
+            $data = $this->cf->createCardFromEstablishmentCode($codeEstablishment);
         } catch (\Exception $e) {
             echo "Une exception a été levé dans le fichier [".$e->getFile()."][ligne : ".$e->getLine()."].".PHP_EOL;
             echo $e->getMessage().PHP_EOL.$e->getCode().PHP_EOL;
